7df551ec658ba16f5470c36e6f75f18278d91d4b
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2020-02-28 12:29:47 +0900

    Change ctags on macos

--- macos.sh
+++ macos.sh
@@ -2,3 +2,5 @@ export LDFLAGS="-L/usr/local/opt/zlib/lib"
 export CPPFLAGS="-I/usr/local/opt/zlib/include"
 
 alias ls='ls -G'
+
+alias ctags=/usr/local/Cellar/universal-ctags/HEAD-003c574/bin/ctags
